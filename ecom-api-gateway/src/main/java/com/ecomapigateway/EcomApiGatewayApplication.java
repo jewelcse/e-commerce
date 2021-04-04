b1c1ba8d91e8d94d51f23853c4965e79d62536cc
jewelcse <jewelcse045@gmial.com>
2021-04-04 16:04:21 +0600

    zuul api gateway intialize

---
+++ ecom-api-gateway/src/main/java/com/ecomapigateway/EcomApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ecomapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApiGatewayApplication.class, args);
+	}
+
+}
---
+++ ecom-api-gateway/src/test/java/com/ecomapigateway/EcomApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
