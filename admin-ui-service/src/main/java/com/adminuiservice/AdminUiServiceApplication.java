da7f73b48715660f044d35c22221a1550d6cd52a
jewelcse <jewelcse045@gmial.com>
2021-04-10 17:34:50 +0600

    registering services with eureka server

--- admin-ui-service/src/main/java/com/adminuiservice/AdminUiServiceApplication.java
+++ admin-ui-service/src/main/java/com/adminuiservice/AdminUiServiceApplication.java
@@ -3,6 +3,7 @@ package com.adminuiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableHystrix
+@EnableDiscoveryClient
 public class AdminUiServiceApplication {
 
 	public static void main(String[] args) {
--- ecom-api-gateway/src/main/java/com/ecomapigateway/EcomApiGatewayApplication.java
+++ ecom-api-gateway/src/main/java/com/ecomapigateway/EcomApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.ecomapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+
+@EnableDiscoveryClient
 public class EcomApiGatewayApplication {
 
 	public static void main(String[] args) {
