c58e32e366cd448387124083e6a31ffb94d7fc54
jewelcse <jewelcse045@gmial.com>
2021-04-18 23:17:09 +0600

    install react for UI service

--- ecom-api-gateway/src/main/java/com/ecomapigateway/EcomApiGatewayApplication.java
+++ ecom-api-gateway/src/main/java/com/ecomapigateway/EcomApiGatewayApplication.java
@@ -1,17 +1,46 @@
 package com.ecomapigateway;
 
+import com.ecomapigateway.filters.ErrorFilter;
+import com.ecomapigateway.filters.PostFilter;
+import com.ecomapigateway.filters.PreFilter;
+import com.ecomapigateway.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class EcomApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcomApiGatewayApplication.class, args);
 	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+
 
 }
